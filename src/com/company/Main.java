0c1331d868d0f1458628f660d339493adc0606bd
Kochevnikbbb <bbelekov@mail.ru>
2021-11-09 03:59:06 +0600

    исправил sout в мейн // добавил еще один раюотающий цикл в будущем понадобится

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,10 +12,13 @@ public class Main {
         System.out.println("Сатана " + satana.getHealth() + "XP " + satana.getDamage()+ " DD " + satana.getShield()+" PP");
 
         Hero[] dc = createHeroes();
+
         for (int i = 0; i < dc.length; i++) {
             System.out.println(dc[i].getName() + " - " + dc[i].getHealth() + "хп " + dc[i].getDamage() + "дд суперспособность: " + dc[i].getPower());
-
         }
+/*        for (Hero hero: dc) {
+            System.out.println(hero.getName() + " - " + hero.getHealth() + "хп " + hero.getDamage() + "дд суперспособность: " + hero.getPower());
+        }*/ //этот цикл тоже работает заебись
 
 
     }
